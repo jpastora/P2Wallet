fac667deec1b9f03884babb5bcaf76890dd25221
Joseph Pastora <joseph.pastora@gmail.com>
2025-07-03 20:22:28 -0600

    Implement user retrieval and update methods
    - Modified `RetrieveById` to use `UserID` and added implementation to fetch a user from the database.
    - Implemented `Update` method to update user details using a stored procedure, replacing the previous placeholder.
    - Added comments to clarify the parameters used in the SQL operation for the `Update` method.

--- DataAccess/CRUD/UserCrudFactory.cs
+++ DataAccess/CRUD/UserCrudFactory.cs
@@ -56,13 +56,60 @@ namespace DataAccess.CRUD
             }
             return lstUsers;
         }
-        public override T RetrieveById<T>(int iD)
+        public override T RetrieveById<T>(int UserID)
         {
-            throw new NotImplementedException();
+            var sqlOperation = new SqlOperation() { ProcedureName = "RET_USER_BY_ID_PR" };
+            sqlOperation.AddIntParam("P_UserID", UserID);
+
+            var lstResults = _sqlDao.ExecuteQueryProcedure(sqlOperation);
+
+            if (lstResults.Count > 0)
+            {
+                var row = lstResults[0];
+                var user = BuildUser(row);
+
+                return (T)Convert.ChangeType(user, typeof(T));
+            }
+            return default(T);
         }
+
+        /*
+         *         FullName = @P_FullName,
+        Email = @P_Email,
+        MobilePhone = @P_MobilePhone,
+        ProfilePhoto = @P_ProfilePhoto,
+        IDPhotoFront = @P_IDPhotoFront,
+        IDPhotoBack = @P_IDPhotoBack,
+        Latitude = @P_Latitude,
+        Longitude = @P_Longitude,
+        Password = @P_Password,
+        EmailVerified = @P_EmailVerified,
+        MobileVerified = @P_MobileVerified,
+        BiometricVerified = @P_BiometricVerified,
+        UserStatus = @P_UserStatus,
+        UpdatedAt = GETDATE()
+            */
+
         public override void Update(BaseDTO baseDTO)
         {
-            throw new NotImplementedException();
+            var user = baseDTO as User;
+            var sqlOperation = new SqlOperation() { ProcedureName = "UPDATE_USER_PR" };
+            sqlOperation.AddIntParam("@P_UserID", user.UserId);
+            sqlOperation.AddStringParameter("@P_FullName", user.FullName);
+            sqlOperation.AddStringParameter("@P_Email", user.Email);
+            sqlOperation.AddStringParameter("@P_MobilePhone", user.MobilePhone);
+            sqlOperation.AddStringParameter("@P_ProfilePhoto", user.ProfilePhoto);
+            sqlOperation.AddStringParameter("@P_IDPhotoFront", user.IDPhotoFront);
+            sqlOperation.AddStringParameter("@P_IDPhotoBack", user.IDPhotoBack);
+            sqlOperation.AddDoubleParam("@P_Latitude", user.Latitude);
+            sqlOperation.AddDoubleParam("@P_Longitude", user.Longitude);
+            sqlOperation.AddStringParameter("@P_Password", user.Password);
+            sqlOperation.AddBooleanParameter("@P_EmailVerified", user.EmailVerified);
+            sqlOperation.AddBooleanParameter("@P_MobileVerified", user.MobileVerified);
+            sqlOperation.AddBooleanParameter("@P_BiometricVerified", user.BiometricVerified);
+            sqlOperation.AddBooleanParameter("@P_UserStatus", user.UserStatus);
+            sqlOperation.AddDateTimeParam("@P_UpdatedAt", DateTime.Now);
+            _sqlDao.ExecuteProcedure(sqlOperation);
         }
         private User BuildUser(Dictionary<string, object> row)
         {
