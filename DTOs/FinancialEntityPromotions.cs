87d24fa00ad968619ff2a3b72a182c672163b80e
Felix <Felix@DESKTOP-CAPCDVL>
2025-07-07 15:19:57 -0600

    Actualizacion de DTO, CrudFactory, SP, Controladores funcionales para MerchantPromotions y FinancialEntityPromotions.

---
+++ DTOs/FinancialEntityPromotions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DTOs
+{
+    public class FinancialEntityPromotions : BaseDTO
+    {
+        public int PromotionID { get; set; }
+        public int FinancialEntityID { get; set; }
+        public string PromotionType { get; set; } 
+        public decimal DiscountPercentage { get; set; }
+        public decimal MaxRefund { get; set; } 
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? AvailableQuantity { get; set; }
+        public bool Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
