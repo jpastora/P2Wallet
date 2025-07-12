bb5cdaca5c5775b957b7704afd8c3f2ab6c75665
Joseph Pastora <joseph.pastora@gmail.com>
2025-07-12 11:37:42 -0600

    Add promotion management classes and CRUD operations
    Introduces `FinancialPromotionManager` and `MerchantPromotionManager` for managing financial and merchant promotions. Adds `FinancialPromotionCrudFactory` and `MerchantPromotionCrudFactory` for database CRUD operations. Defines DTO classes `FinancialPromotion` and `MerchantPromotion` to represent promotion data structures. Enhances the application's ability to manage promotions effectively.

---
+++ CoreApp/MerchantPromotionManager.cs
@@ -0,0 +1,70 @@
+﻿using DataAccess.CRUD;
+using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreApp
+{
+    // Manages operations related to merchant promotions.
+    public class MerchantPromotionManager : BaseManager
+    {
+        // Creates a new merchant promotion.
+        public void CreatePromotion(MerchantPromotion promotion)
+        {
+            try
+            {
+                var crud = new MerchantPromotionCrudFactory();
+                crud.Create(promotion); // Inserts the promotion into the database
+            }
+            catch (Exception ex)
+            {
+                ManageException(ex); // Handles the exception using the base method
+            }
+        }
+
+        // Retrieves all merchant promotions from the system.
+        public List<MerchantPromotion> RetrieveAllPromotions()
+        {
+            var crud = new MerchantPromotionCrudFactory();
+            return crud.RetrieveAll<MerchantPromotion>();
+        }
+
+        // Retrieves a merchant promotion by its ID.
+        public MerchantPromotion RetrievePromotionById(int id)
+        {
+            var crud = new MerchantPromotionCrudFactory();
+            return crud.RetrieveById<MerchantPromotion>(id);
+        }
+
+        // Updates an existing merchant promotion.
+        public void UpdatePromotion(MerchantPromotion promotion)
+        {
+            try
+            {
+                var crud = new MerchantPromotionCrudFactory();
+                crud.Update(promotion); // Updates the promotion in the database
+            }
+            catch (Exception ex)
+            {
+                ManageException(ex);
+            }
+        }
+
+        // Deletes a merchant promotion.
+        public void DeletePromotion(MerchantPromotion promotion)
+        {
+            try
+            {
+                var crud = new MerchantPromotionCrudFactory();
+                crud.Delete(promotion); // Removes the promotion from the database
+            }
+            catch (Exception ex)
+            {
+                ManageException(ex);
+            }
+        }
+    }
+}
---
+++ DataAccess/CRUD/MerchantPromotionCrudFactory.cs
@@ -0,0 +1,108 @@
+﻿using DataAccess.DAO;
+using DataAccess.DAO.DataAccess.DAO;
+using DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.CRUD
+{
+    public class MerchantPromotionCrudFactory : CrudFactory
+    {
+        public MerchantPromotionCrudFactory()
+        {
+            _sqlDao = SqlDAO.GetInstance();
+        }
+
+        public override void Create(BaseDTO baseDTO)
+        {
+            var promo = baseDTO as MerchantPromotion;
+            var operation = new SqlOperation { ProcedureName = "CREATE_MERCHANT_PROMOTION_PR" };
+
+            operation.AddIntParam("@P_MerchantID", promo.MerchantID);
+            operation.AddStringParameter("@P_PromotionType", promo.PromotionType);
+            operation.AddDoubleParam("@P_DiscountPercentage", promo.DiscountPercentage);
+            operation.AddDoubleParam("@P_MaxRefund", promo.MaxRefund);
+            operation.AddDateTimeParam("@P_StartDate", promo.StartDate);
+            operation.AddDateTimeParam("@P_EndDate", promo.EndDate);
+            operation.AddIntParam("@P_AvailableQuantity", promo.AvailableQuantity);
+            operation.AddStringParameter("@P_ValidationStatus", promo.ValidationStatus);
+
+            _sqlDao.ExecuteProcedure(operation);
+        }
+
+        public override void Delete(BaseDTO baseDTO)
+        {
+            var promo = baseDTO as MerchantPromotion;
+            var operation = new SqlOperation { ProcedureName = "DELETE_MERCHANT_PROMOTION_PR" };
+            operation.AddIntParam("@P_PromotionID", promo.ID);
+            _sqlDao.ExecuteProcedure(operation);
+        }
+
+        public override void Update(BaseDTO baseDTO)
+        {
+            var promo = baseDTO as MerchantPromotion;
+            var operation = new SqlOperation { ProcedureName = "UPDATE_MERCHANT_PROMOTION_PR" };
+
+            operation.AddIntParam("@P_PromotionID", promo.ID);
+            operation.AddIntParam("@P_MerchantID", promo.MerchantID);
+            operation.AddStringParameter("@P_PromotionType", promo.PromotionType);
+            operation.AddDoubleParam("@P_DiscountPercentage", promo.DiscountPercentage);
+            operation.AddDoubleParam("@P_MaxRefund", promo.MaxRefund);
+            operation.AddDateTimeParam("@P_StartDate", promo.StartDate);
+            operation.AddDateTimeParam("@P_EndDate", promo.EndDate);
+            operation.AddIntParam("@P_AvailableQuantity", promo.AvailableQuantity);
+            operation.AddStringParameter("@P_ValidationStatus", promo.ValidationStatus);
+
+            _sqlDao.ExecuteProcedure(operation);
+        }
+
+        public override T RetrieveById<T>(int id)
+        {
+            var operation = new SqlOperation { ProcedureName = "RET_MERCHANT_PROMOTION_BY_ID_PR" };
+            operation.AddIntParam("@P_PromotionID", id);
+
+            var result = _sqlDao.ExecuteQueryProcedure(operation);
+            if (result.Count > 0)
+            {
+                var row = result[0];
+                var promo = BuildMerchantPromotion(row);
+                return (T)Convert.ChangeType(promo, typeof(T));
+            }
+            return default(T);
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            var list = new List<T>();
+            var operation = new SqlOperation { ProcedureName = "RET_ALL_MERCHANT_PROMOTIONS_PR" };
+
+            var result = _sqlDao.ExecuteQueryProcedure(operation);
+            foreach (var row in result)
+            {
+                var promo = BuildMerchantPromotion(row);
+                list.Add((T)Convert.ChangeType(promo, typeof(T)));
+            }
+
+            return list;
+        }
+
+        private MerchantPromotion BuildMerchantPromotion(Dictionary<string, object> row)
+        {
+            return new MerchantPromotion
+            {
+                ID = (int)row["PromotionID"],
+                MerchantID = (int)row["MerchantID"],
+                PromotionType = (string)row["PromotionType"],
+                DiscountPercentage = Convert.ToDouble(row["DiscountPercentage"]),
+                MaxRefund = Convert.ToDouble(row["MaxRefund"]),
+                StartDate = Convert.ToDateTime(row["StartDate"]),
+                EndDate = Convert.ToDateTime(row["EndDate"]),
+                AvailableQuantity = Convert.ToInt32(row["AvailableQuantity"]),
+                ValidationStatus = (string)row["ValidationStatus"]
+            };
+        }
+    }
+}
