72185d6c5d45b285fb70239ac8fd49c1e5dd6b4e
Juan Diego Gonzalez Viquez <jgonzalezvi@ucenfotec.ac.cr>
2025-07-12 21:49:48 -0600

    Add CloudinaryController for image uploads
    This commit introduces a new `CloudinaryController` class in the `WebAPI.Controllers` namespace. The controller facilitates image uploads to Cloudinary by providing a `SaveImage` method that processes incoming image files. It retrieves the Cloudinary URL from the configuration and handles file uploads, returning the upload result or an error message as needed.

---
+++ WebAPI/Controllers/CloudinaryController.cs
@@ -0,0 +1,49 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/cloudinary")]
+    [ApiController]
+    public class CloudinaryController : ControllerBase
+    {
+        private string _cloudinaryUrl;
+
+        public CloudinaryController(IConfiguration configuration) 
+        { 
+            _cloudinaryUrl = configuration.GetSection("Cloudinary").GetSection("URL").Value;
+        }
+
+        [HttpPost]
+        [Route("save")]
+        public async Task<ActionResult> SaveImage(IFormFile photo)
+        {
+            try
+            {
+                Cloudinary cloudinary = new Cloudinary(_cloudinaryUrl);
+                var fileName = photo.FileName;
+                var fileWithPath = Path.Combine("Uploads", fileName);
+                var stream = new FileStream(fileWithPath, FileMode.Create);
+                photo.CopyTo(stream);
+                stream.Close();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(fileWithPath),
+                    UseFilename = true,
+                    Overwrite = true,
+                    Folder = "Yavi"
+                };
+                var uploadResult = await cloudinary.UploadAsync(uploadParams);
+                System.IO.File.Delete(fileWithPath); // Limpia el archivo local después de subirlo
+                return Ok(uploadResult);    
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
