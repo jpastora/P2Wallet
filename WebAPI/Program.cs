87e552c49b94bf8e1f6046b68c9d86f25f64a922
Juan Diego Gonzalez Viquez <jgonzalezvi@ucenfotec.ac.cr>
2025-07-14 22:14:42 -0600

    Update CORS policy and handle image upload response
    Modified CORS policy in `Program.cs` to allow requests from a specific frontend URL and uncommented the CORS middleware.
    
    In `UserRegister.cshtml`, added functionality to handle the response from the Cloudinary fetch request, extracting the `secureUrl` and assigning it to the input field for better user experience.
    
    Add CORS policy and enhance image upload handling
    
    - Introduced a new CORS policy "AllowAll" in Program.cs, replacing the old "misOrigenes" policy and updating middleware configuration.
    - Updated UserRegister.cshtml to capture and store the uploaded image's URL from the Cloudinary API response.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,7 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Define un nombre para la política de CORS
-var misOrigenes = "misOrigenes";
+/*var misOrigenes = "misOrigenes";
 
 // Agrega el servicio de CORS
 builder.Services.AddCors(options =>
@@ -10,11 +20,11 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           // Permite solicitudes desde tu frontend local
-                          policy.WithOrigins("https://p2wallet-webapp-e9h3c6c8gtdxaeg3.canadacentral-01.azurewebsites.net/")
+                          policy.WithOrigins("https://p2wallet-webapp-e9h3c6c8gtdxaeg3.canadacentral-01.azurewebsites.net/*")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
-});
+});*/
 
 // Add services to the container.
 
@@ -24,14 +34,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors("AllowAll");
+//app.UseCors("AllowFrontendApp");
 
 // Configure the HTTP request pipeline.
-    app.UseSwagger();
+app.UseSwagger();
     app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.UseCors(misOrigenes);
+//app.UseCors(misOrigenes);
 
 app.UseAuthorization();
 
