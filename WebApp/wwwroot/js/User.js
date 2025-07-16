760149ab9e07f5f6ed83aacf17af901044fe97aa
Joseph Pastora <joseph.pastora@gmail.com>
2025-07-16 14:50:48 -0600

    Add profile photo URL handling and fix syntax error
    - Added a new parameter for the user's profile photo URL in `UserCrudFactory.cs` to enable storage in the database.
    - Corrected a syntax error in `User.js` by replacing a comma with a semicolon in the initialization of `userDTO.ProfilePhotoUrl`.

--- WebApp/wwwroot/js/User.js
+++ WebApp/wwwroot/js/User.js
@@ -20,7 +20,7 @@
         userDTO.IDNumber = ""
         userDTO.Created = "2000-01-01";
         userDTO.Updated = "2000-01-01";
-        userDTO.ProfilePhotoUrl = "https://res.cloudinary.com/dxn6cnvz7/image/upload/v1752335713/cld-sample-5.jpg",
+        userDTO.ProfilePhotoUrl = "https://res.cloudinary.com/dxn6cnvz7/image/upload/v1752335713/cld-sample-5.jpg";
         userDTO.IDPhotoFrontUrl = "https://res.cloudinary.com/dxn6cnvz7/image/upload/v1752335713/cld-sample-5.jpg";
         userDTO.IDPhotoBackUrl = "https://res.cloudinary.com/dxn6cnvz7/image/upload/v1752335713/cld-sample-5.jpg";
         userDTO.EmailVerified = "Active";
